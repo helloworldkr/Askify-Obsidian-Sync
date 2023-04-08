061af210ab58093a7d59b07cc82a156c0cc09651
kishlay raj <kishlayraj@kishlays-MacBook-Pro.local>
2023-04-08 13:14:30 +0530

    made changes as per the MR requirements

--- main.ts
+++ main.ts
@@ -8,7 +8,9 @@ import {
 	PluginSettingTab,
 	Setting,
 	TFile,
+	addIcon,
 	Vault,
+	requestUrl
 } from 'obsidian';
 // import { store } from 'obsidian';
 var request = require('request');
@@ -19,13 +21,14 @@ const path = require('path');
 const fs = require('fs');
 
 
+
 // Remember to rename these classes and interfaces!
 
-interface MyPluginSettings {
+interface AskifyPluginSettings {
 	mySetting: string;
 }
 
-const DEFAULT_SETTINGS: MyPluginSettings = {
+const DEFAULT_SETTINGS: AskifyPluginSettings = {
 	mySetting: 'default'
 }
 
@@ -46,13 +49,16 @@ async function unzipFile(filePath, destPath) {
 }
 
 export default class MyPlugin extends Plugin {
-	settings: MyPluginSettings;
+	settings: AskifyPluginSettings;
 
 	async onload() {
 		console.log("plugin loadded..");
 		await this.loadSettings();
 		// This creates an icon in the left ribbon.
-		const ribbonIconEl = this.addRibbonIcon('dice', 'Askify Sync Plugin', async (evt: MouseEvent) => {
+		// addIcon("sync", `<sync    <circle cx="50" cy="50" r="50" fill="currentColor" />`);
+		addIcon("circle", `<circle fill="#000000" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"><path d="M19.91,15.51H15.38a1,1,0,0,0,0,2h2.4A8,8,0,0,1,4,12a1,1,0,0,0-2,0,10,10,0,0,0,16.88,7.23V21a1,1,0,0,0,2,0V16.5A1,1,0,0,0,19.91,15.51ZM12,2A10,10,0,0,0,5.12,4.77V3a1,1,0,0,0-2,0V7.5a1,1,0,0,0,1,1h4.5a1,1,0,0,0,0-2H6.22A8,8,0,0,1,20,12a1,1,0,0,0,2,0A10,10,0,0,0,12,2Z"></path></g>`);
+		console.log("trying to load the ribbion")
+		const ribbonIconEl = this.addRibbonIcon('circle', 'Askify Sync Plugin', async (evt: MouseEvent) => {
 			// Called when the user clicks the icon.
 			new Notice('Sync started');
 
@@ -120,65 +126,13 @@ export default class MyPlugin extends Plugin {
 
 			new Notice('Sync complete');
 		});
-		// Perform additional things with the ribbon
-		ribbonIconEl.addClass('my-plugin-ribbon-class');
-
-		// This adds a status bar item to the bottom of the app. Does not work on mobile apps.
-		// const statusBarItemEl = this.addStatusBarItem();
-		// statusBarItemEl.setText('Status Bar Text');
-
-		// This adds a simple command that can be triggered anywhere
-		this.addCommand({
-			id: 'open-sample-modal-simple',
-			name: 'Open sample modal (simple)',
-			callback: () => {
-				new SampleModal(this.app).open();
-			}
-		});
-		// This adds an editor command that can perform some operation on the current editor instance
-		this.addCommand({
-			id: 'sample-editor-command',
-			name: 'Sample editor command',
-			editorCallback: (editor: Editor, view: MarkdownView) => {
-				console.log(editor.getSelection());
-				editor.replaceSelection('Sample Editor Command');
-			}
-		});
-		// This adds a complex command that can check whether the current state of the app allows execution of the command
-		this.addCommand({
-			id: 'open-sample-modal-complex',
-			name: 'Open sample modal (complex)',
-			checkCallback: (checking: boolean) => {
-				// Conditions to check
-				const markdownView = this.app.workspace.getActiveViewOfType(MarkdownView);
-				if (markdownView) {
-					// If checking is true, we're simply "checking" if the command can be run.
-					// If checking is false, then we want to actually perform the operation.
-					if (!checking) {
-						new SampleModal(this.app).open();
-					}
-
-					// This command will only show up in Command Palette when the check function returns true
-					return true;
-				}
-			}
-		});
-
-		// This adds a settings tab so the user can configure various aspects of the plugin
-		this.addSettingTab(new SampleSettingTab(this.app, this));
-
-		// If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
-		// Using this function will automatically remove the event listener when this plugin is disabled.
-		this.registerDomEvent(document, 'click', (evt: MouseEvent) => {
-			console.log('click', evt);
-		});
-
-		// When registering intervals, this function will automatically clear the interval when the plugin is disabled.
-		this.registerInterval(window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000));
+		
+		
+	
 	}
 
 	private async getNotesZipFileName(apikey) {
-		var axios = require('axios');
+		// var axios = require('axios');
 		var data = JSON.stringify({
 			"apiKey": apikey
 		});
@@ -192,33 +146,45 @@ export default class MyPlugin extends Plugin {
 			data: data
 		};
 
-		let resp = await axios(config)
+		let resp = await requestUrl(config);
 		console.log("resp is ");
 		console.log(resp);
-		return resp.data;
+		console.log("json response is ");
+		console.log(resp.json);
+		return resp.json;
 	}
 
-	private downloadAskifyNotesAsZip(vault, fileUrl, filename) {
-		return new Promise((resolve) => {
-			console.log("starting the download")
-			https.get(fileUrl).on('response', function (response) {
-				// var body = '';
-				let binaryData = Buffer.alloc(0);
-				var i = 0;
-				response.on('data', function (chunk) {
-					i++;
-					// body += chunk;
-					binaryData = Buffer.concat([binaryData, chunk]);
-					console.log('BODY Part: ' + i);
-				});
-				response.on('end', async function () {
-					// console.log(body);
-					console.log('Finished');
-					// fs.writeFileSync('test5.zip', binaryData);
-					await vault.createBinary(filename, binaryData);
-					resolve("success")
-				});
-			});
+	private downloadAskifyNotesAsZip(vault, fileUrl, fileName) {
+		// return new Promise((resolve) => {
+		// 	console.log("starting the download")
+		// 	https.get(fileUrl).on('response', function (response) {
+		// 		// var body = '';
+		// 		let binaryData = Buffer.alloc(0);
+		// 		var i = 0;
+		// 		response.on('data', function (chunk) {
+		// 			i++;
+		// 			// body += chunk;
+		// 			binaryData = Buffer.concat([binaryData, chunk]);
+		// 			console.log('BODY Part: ' + i);
+		// 		});
+		// 		response.on('end', async function () {
+		// 			// console.log(body);
+		// 			console.log('Finished');
+		// 			// fs.writeFileSync('test5.zip', binaryData);
+		// 			await vault.createBinary(filename, binaryData);
+		// 			resolve("success")
+		// 		});
+		// 	});
+		// });
+
+		let fileData: ArrayBuffer; 
+		return new Promise(async (resolve) => {
+			console.log("starting the download");
+			fileData = await requestUrl({url: fileUrl}).arrayBuffer;
+
+			if(fileData!=null){
+				vault.createBinary(fileName, fileData);
+			}
 		});
 	}
 
